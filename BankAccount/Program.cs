98d54ee67ff189cac91d92795a85dbabd31b0c5f
TheNimum <mikael.pajari@live.se>
2021-02-25 15:13:22 +0100

    uppdated List

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using static System.Console;
+using System.Linq;
 
 namespace BankAccount
 {
-    
+    public class TransactionInfo
+    {
+        public TransactionInfo(float Out_Withdraw, float In_Deposit)
+        {
+           this.Withdraw = Out_Withdraw;
+           this.Deposit = In_Deposit;
+           this.TimeStamps = DateTime.Now;
+        }
+
+       public float Withdraw { get; set; }
+       public float Deposit { get; set; }
+       public override string ToString()
+        {
+            return null;
+        }
+       public DateTime TimeStamps { get; set; }
+        
+    }
     public interface IBankAccount
     {
         void Withdraw(float amount);
@@ -14,10 +32,12 @@ namespace BankAccount
         string GetBalanceInformation();
         List<TransactionInfo> GetTransactionList();
     }
-    
+
+   
+
     public abstract class BankAccount : IBankAccount
     {
-        
+       protected List<TransactionInfo> TS = new List<TransactionInfo>();
         public float Balance;
         public BankAccount(float balance)
         {
@@ -27,10 +47,18 @@ namespace BankAccount
         public virtual void Withdraw(float amount)
         {
             this.Balance -= amount;
+            TS.Add(new TransactionInfo(amount, 0));
+            
+           
+            
         }
         public virtual void Deposit (float amount)
         {
             this.Balance += amount;
+            TS.Add(new TransactionInfo(0, amount));
+            
+            
+            
         }
 
         public float GetBalance()
@@ -45,13 +73,15 @@ namespace BankAccount
 
         public List<TransactionInfo> GetTransactionList()
         {
-            throw new NotImplementedException();
+
+            return TS; 
         }
     }
     
 
     class PremiumBankAccount : BankAccount
     {
+        
         public PremiumBankAccount(int balance) : base(balance)
         {
         }
@@ -62,6 +92,7 @@ namespace BankAccount
         public override void Deposit(float amount)
         {
             base.Deposit(amount);
+            TS.Add(new TransactionInfo(0, amount));
         }
         
     }
@@ -75,6 +106,7 @@ namespace BankAccount
         {
             int DebitFee = 250;
             base.Withdraw(amount + DebitFee);
+            TS.Add(new TransactionInfo(amount + DebitFee,0));
         }
         public override void Deposit(float amount)
         {
@@ -94,9 +126,9 @@ namespace BankAccount
                 string BalanceInformation = PremiumCostumer.GetBalanceInformation();
                 float CurrentSaldoNormal = NormalCostumer.GetBalance();
                 string NormalBalanceInformation = NormalCostumer.GetBalanceInformation();
-
+                var test = PremiumCostumer.GetTransactionList();
                 WriteLine(
-                    "Premium kundens saldo " + BalanceInformation + "\nNormal Kundens Saldo " + NormalBalanceInformation);
+                    "Premium kundens saldo " + BalanceInformation + "\nNormal Kundens Saldo " + NormalBalanceInformation + "\n" + test);
                 ReadKey();
             }
         }
